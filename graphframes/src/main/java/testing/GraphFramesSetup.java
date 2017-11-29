5b6de188e97137d71a029163a556eb1bd468832d
Santiago Ocamica <santi6982@gmail.com>
2017-11-29 17:56:44 -0300

    Better configuration with constants

--- graphframes/src/main/java/testing/GraphFramesSetup.java
+++ graphframes/src/main/java/testing/GraphFramesSetup.java
@@ -11,6 +11,12 @@ import org.graphframes.GraphFrame;
 import static org.apache.spark.sql.functions.*;
 
 public class GraphFramesSetup {
+    private static String CALLS_FILE_LOCATION = "/user/socamica/llamadas800K.csv";
+    private static String PHONES_FILE_LOCATION = "/user/socamica/telefonos800K.csv";
+    private static String VERTICES_PARQUET_LOCATION = "/user/socamica/grupo1v2-vertices";
+    private static String EDGES_PARQUET_LOCATION = "/user/socamica/grupo1v2-edges";
+    private static boolean BUILD_GRAPH = false;
+    private static String QUERY_NUMBER = "1_1";
     private static List<Row> readCSV(String location, SparkSession session, int offset, int limit) throws Exception {
         Dataset<Row> csv = session.read().csv(location);
         return csv.filter(new FilterFunction<Row>(){
@@ -28,28 +34,28 @@ public class GraphFramesSetup {
         int limit = 100000;
         List<Row> chunkPhones;
         List<Row> chunkCalls;
-        chunkPhones = readCSV("/user/socamica/telefonos800K.csv", session, offset, limit);
+        chunkPhones = readCSV(PHONES_FILE_LOCATION, session, offset, limit);
         SubGraph telefonosGraph = GraphBuilder.buildTelefonos(chunkPhones);
         Dataset<Row> verticesDF = telefonosGraph.getVertices(context);
         Dataset<Row> edgesDF = telefonosGraph.getEdges(context);
-        chunkCalls = readCSV("/user/socamica/llamadas800K.csv", session, offset, limit);
+        chunkCalls = readCSV(CALLS_FILE_LOCATION, session, offset, limit);
         while(chunkCalls.size() > 0) {
             SubGraph llamadasGraph = GraphBuilder.buildLlamadas(chunkCalls);
             verticesDF = verticesDF.union(llamadasGraph.getVertices(context));
             edgesDF = edgesDF.union(llamadasGraph.getEdges(context));
             offset += limit;
-            chunkCalls = readCSV("/user/socamica/llamadas800K.csv", session, offset, limit);
+            chunkCalls = readCSV(CALLS_FILE_LOCATION, session, offset, limit);
         }
-        edgesDF.write().save("/user/socamica/grupo1v2-edges");
-        verticesDF.write().save("/user/socamica/grupo1v2-vertices");
         // Persist data
+        edgesDF.write().save(EDGES_PARQUET_LOCATION);
+        verticesDF.write().save(VERTICES_PARQUET_LOCATION);
         return GraphFrame.apply(verticesDF, edgesDF);
     }
 
     public static GraphFrame loadGraph(SparkSession session) {
         // Recover persisted data
-        Dataset<Row> edgesDF = session.read().load("/user/socamica/grupo1v2-edges");
-        Dataset<Row> verticesDF = session.read().load("/user/socamica/grupo1v2-vertices");
+        Dataset<Row> edgesDF = session.read().load(EDGES_PARQUET_LOCATION);
+        Dataset<Row> verticesDF = session.read().load(VERTICES_PARQUET_LOCATION);
         return GraphFrame.apply(verticesDF, edgesDF);
     }
 
@@ -65,24 +71,95 @@ public class GraphFramesSetup {
         System.out.println("[TOTAL] Average: " + time / 5.0);
     }
 
+    public static Dataset<Row> runQuery(GraphFrame graph, String number) {
+        Dataset<Row> calls;
+        switch(number) {
+            case "1_1":
+                calls = graph.find("(t1)-[]->(l); (t2)-[]->(l)");
+                return calls.filter("l.type = 'llamada'")
+                            .filter("t1.type = 'telefono'")
+                            .filter("t2.type = 'telefono'")
+                            .filter("t1.numero > t2.numero")
+                            .groupBy("t1.numero", "t2.numero")
+                            .agg(avg(calls.col("l.duration")));
+            case "1_2":
+                calls = graph.find("(t1)-[e1]->(l); (t2)-[e2]->(l)");
+                return calls.filter("l.type = 'llamada'")
+                            .filter("t1.type = 'telefono'")
+                            .filter("t2.type = 'telefono'")
+                            .filter("t1.id > t2.id")
+                            .filter("e1.type = 'creo'")
+                            .filter("e2.type = 'recibio'")
+                            .groupBy("t1.id", "t2.id")
+                            .agg(avg(calls.col("l.duration")));
+            case "1_3":
+                calls = graph.find("(t1)-[]->(l); (t2)-[]->(l)");
+                return calls.filter("l.type = 'llamada'")
+                            .filter("t1.type = 'telefono'")
+                            .filter("t2.type = 'telefono'")
+                            .filter("t1.numero > t2.numero")
+                            .groupBy("t1.numero", "t2.numero")
+                            .agg(max(calls.col("l.duration")));
+            case "1_4":
+                calls = graph.find("(t1)-[]->(l); (t2)-[]->(l); (u1)-[]->(t1); (u2)-[]->(t2)");
+                return calls.filter("l.type = 'llamada'")
+                            .filter("t1.type = 'telefono'")
+                            .filter("t2.type = 'telefono'")
+                            .filter("u1.id > u2.id")
+                            .groupBy("u1.id", "u2.id")
+                            .agg(countDistinct(calls.col("l.id").as("cantidad_llamadas")));
+            case "1_5":
+                calls = graph.find("(t1)-[]->(l); (t2)-[]->(l); (u1)-[]->(t1); (u2)-[]->(t2)");
+                return calls.filter("l.type = 'llamada'")
+                            .filter("t1.type = 'telefono'")
+                            .filter("t2.type = 'telefono'")
+                            .filter("u1.id > u2.id")
+                            .groupBy(calls.col("u1.id"), calls.col("u2.id"),
+                                     month(calls.col("l.startTime")).as("mes"),
+                                     year(calls.col("l.startTime")).as("año"))
+                            .agg(countDistinct(calls.col("l.id")).as("cantidad_llamadas"));
+            case "1_6":
+                calls = graph.find("(t1)-[]->(l); (t2)-[]->(l); (u1)-[]->(t1); (u2)-[]->(t2)");
+                return calls.filter("l.type = 'llamada'")
+                            .filter(month(calls.col("l.startTime")).$eq$eq$eq(9))
+                            .filter("t1.type = 'telefono'")
+                            .filter("t2.type = 'telefono'")
+                            .filter("u1.id > u2.id")
+                            .groupBy(calls.col("u1.id"), calls.col("u2.id"))
+                            .agg(countDistinct(calls.col("l.id")).as("cantidad_llamadas"));
+            case "2_1":
+                calls = graph.find("(t1)-[]->(l); (t2)-[]->(l); (t3)-[]->(l); (u1)-[]->(t1); (u2)-[]->(t2); (u3)-[]->(t3)");
+                return calls.filter("l.type = 'llamada'")
+                            .filter(month(calls.col("l.startTime")).$eq$eq$eq(9))
+                            .filter(year(calls.col("l.startTime")).$eq$eq$eq(2017))
+                            .filter("t1.type = 'telefono'")
+                            .filter("t2.type = 'telefono'")
+                            .filter("t3.type = 'telefono'")
+                            .filter("u1.id > u2.id")
+                            .filter("u2.id > u3.id")
+                            .groupBy("u1.id", "u2.id", "u3.id")
+                            .agg(avg(calls.col("l.duration")));
+            default: return null;
+        }
+    }
+
     public static void main(String[] args) throws Exception {
         SparkSession sp = SparkSession
             .builder()
             .appName("TPE Grupo 1")
             .getOrCreate();
         
-        GraphFrame myGraph = loadGraph(sp);
-        benchmark(() -> {
-            Dataset<Row> calls = myGraph.find("(t1)-[]->(l); (t2)-[]->(l); (u1)-[]->(t1); (u2)-[]->(t2)");
-            Dataset<Row> result = calls.filter("l.type = 'llamada'")
-                                       .filter(month(calls.col("l.startTime")).$eq$eq$eq(9))
-                                       .filter("t1.type = 'telefono'")
-                                       .filter("t2.type = 'telefono'")
-                                       .filter("u1.id > u2.id")
-                                       .groupBy(calls.col("u1.id"), calls.col("u2.id"))
-                                       .agg(countDistinct(calls.col("l.id")).as("cantidad_llamadas"));
-            result.show();
-        });
+            GraphFrame myGraph;
+        if (BUILD_GRAPH) {
+            myGraph = buildGraph(sp);
+        } else {
+            myGraph = loadGraph(sp);
+        }
+        long start = System.currentTimeMillis();
+        Dataset<Row> result = runQuery(myGraph, QUERY_NUMBER);
+        long elapsed = System.currentTimeMillis() - start;
+        System.out.println("The query took " + elapsed + " ms.");
+        result.show();
         sp.close();
     }
 }
